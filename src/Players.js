2a8800f611b189a0f3b36ea8a5d4d10cf30c4975
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-01-13 19:48:26 +0300

    An array of different players lists with theirrespective details

---
+++ src/Players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Inter Miami",
+      nationality: "Argentina",
+      jerseyNumber: 10,
+      age: 36,
+      imageUrl: "https://images.app.goo.gl/YtEmQS3KmkFJ14Rb6",
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Al-Nassr",
+      nationality: "Portugal",
+      jerseyNumber: 7,
+      age: 38,
+      imageUrl: "https://images.app.goo.gl/4tibdC8p9jXsV9Pn7",
+    },
+    {
+      name: "Kylian Mbappé",
+      team: "Paris Saint-Germain",
+      nationality: "France",
+      jerseyNumber: 7,
+      age: 24,
+      imageUrl: "https://images.app.goo.gl/fmAY8kd5SGxB93pG9",
+    },
+    {
+      name: "Bukayo Saka",
+      team: "Arsenal",
+      nationality: "England",
+      jerseyNumber: 7,
+      age: 22,
+      imageUrl: "https://images.app.goo.gl/qZuK1GBYEzvXsMKS6",
+    },
+  ];
+  
+  export default players;
+  
