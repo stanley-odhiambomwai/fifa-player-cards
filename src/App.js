0ec8548ffc0bbbe190344324abb6d0b03dbec3d3
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-01-13 20:15:06 +0300

    updating the app.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import PlayersList from './PlayersList';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return(
+  <div className="App">
+  <h1 style={{ textAlign: 'center', margin: '20px' }}>FIFA Player Cards</h1>
+  <PlayersList />
+</div>
+ )
 }
 
 export default App;
